c15eedce7541399c9bccc5600732feb4b75eb61d
kavita.sonawane@t-systems.com <kavita.sonawane@t-systems.com>
2021-11-02 19:26:50 +0530

    Change image path

--- templates/personal-settings.php
+++ templates/personal-settings.php
@@ -31,21 +31,21 @@ script('nmc-firstrunwizard', ['personalsettings']);
 	<h2><?php p($l->t('Desktop & mobile clients'));?></h2>
 	<div class="clientslinks">
 		<a href="https://static.magentacloud.de/software/MagentaCLOUD.exe" rel="noreferrer noopener" target="_blank">
-			<img src="<?php print_unescaped(image_path('core', 'WinOS.svg')); ?>"
+			<img src="<?php print_unescaped(image_path('apps', 'WinOS.svg')); ?>"
 				 alt="<?php p($l->t('Desktop client'));?>"/>
 		</a>
 		<a href="https://app.adjust.com/r4e1yl" rel="noreferrer noopener" target="_blank">
-			<img src="<?php print_unescaped(image_path('core', 'Google-Play-Store.svg')); ?>"
+			<img src="<?php print_unescaped(image_path('apps', 'Google-Play-Store.svg')); ?>"
 				 alt="<?php p($l->t('Android app'));?>" />
 		</a>
 	</div>
 	<div id="clientslinks">
 		<a href="" rel="noreferrer noopener" target="_blank">
-			<img src="<?php print_unescaped(image_path('core', 'iOS.svg')); ?>"
+			<img src="<?php print_unescaped(image_path('apps', 'iOS.svg')); ?>"
 				 alt="<?php p($l->t('iOS app'));?>" />
 		</a>
 		<a href="https://static.magentacloud.de/software/MagentaCLOUD.dmg" rel="noreferrer noopener" target="_blank">
-			<img src="<?php print_unescaped(image_path('core', 'MacOS.svg')); ?>"
+			<img src="<?php print_unescaped(image_path('apps', 'MacOS.svg')); ?>"
 				 alt="<?php p($l->t('mac os'));?>" />
 		</a>
 	</div>
